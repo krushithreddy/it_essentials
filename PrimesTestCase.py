3ad4b91b9f225231d4cb87b58cc4e40a48308210
krushithreddy <34689161+krushithreddy@users.noreply.github.com>
2023-02-02 09:09:26 -0700

    Update PrimesTestCase.py

--- PrimesTestCase.py
+++ PrimesTestCase.py
@@ -1,6 +1,6 @@
-import unittest 
+#import unittest 
 from prime import is_prime 
-
+import unittest
 class PrimesTestCase(unittest.TestCase):
    def test_is_five_prime(self):    
       self.assertTrue(is_prime(5)) 
